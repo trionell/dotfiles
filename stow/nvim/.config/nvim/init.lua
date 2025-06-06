9862877d7a5ce391c45eaf034723ae125a5bd4e7
Gustav Råkeberg <gustav.rakeberg@gmail.com>
2025-06-06 21:56:25 +0200

    Added stow dotfiles for nvim, ghostty & bash

---
+++ stow/nvim/.config/nvim/init.lua
@@ -0,0 +1,36 @@
+vim.o.number = true
+
+vim.o.mouse = 'a'
+
+vim.o.tabstop = 4
+vim.o.expandtab = true
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+
+-- Sync clipboard between OS and Neovim
+vim.schedule(function()
+    vim.o.clipboard = 'unnamedplus'
+end)
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Preview substitutions live, as you type
+vim.o.inccommand = 'split'
+
+-- Show which line the cursor is on
+vim.o.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor
+vim.o.scrolloff = 10
+
+-- If performing an operation that would fail due to unsaved changes in the buffer (like ':q'),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.o.confirm = true
+
+vim.opt.termguicolors = true
