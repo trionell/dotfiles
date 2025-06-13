8f70a86bc096bb24def211474fb6f442c2952495
Gustav Råkeberg <gustav.rakeberg@gmail.com>
2025-06-13 22:00:47 +0200

    Added dos2unix to pacman install script

--- scripts/installPacmanPackages.sh
+++ scripts/installPacmanPackages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 pacman -Syu --noconfirm
-pacman -S git stow ghostty firefox code neovim less base-devel tree jdk21-openjdk maven podman podman-compose vlc nvm --needed --noconfirm
+pacman -S git stow ghostty firefox code neovim less base-devel tree jdk21-openjdk maven podman podman-compose vlc nvm dos2unix --needed --noconfirm
