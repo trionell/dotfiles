48db4b5987f5a501c911b02ed24ba13e4172f5e2
Gustav Råkeberg <gustav.rakeberg@gmail.com>
2026-03-21 22:17:43 +0100

    Added yazi to pacman install script

--- scripts/installPacmanPackages.sh
+++ scripts/installPacmanPackages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 pacman -Syu --noconfirm
-pacman -S git stow ghostty firefox code neovim less base-devel tree jdk21-openjdk maven podman podman-compose vlc vlc-plugins-all nvm dos2unix kdeconnect lazygit--needed --noconfirm
+pacman -S git stow ghostty firefox code neovim less base-devel tree jdk21-openjdk maven podman podman-compose vlc vlc-plugins-all nvm dos2unix kdeconnect lazygit--needed yazi ffmpeg 7zip jq poppler fd ripgrep fzf zoxide resvg imagemagick --noconfirm
