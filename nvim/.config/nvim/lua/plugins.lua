5d520e200789334f76a5af5d40ac4ac0cc50549e
Gustav Råkeberg <gustav.rakeberg@gmail.com>
2026-04-09 22:23:49 +0200

    nvim: Use mini.completion for autocompletion

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -20,6 +20,16 @@ vim.pack.add({
 
 -- Enable plugins
 require("plugins.tiny-inline-diagnostic")
+require("mini.completion").setup()
+vim.o.completeopt = "menu,menuone,noinsert"
+
+-- Confirm completion with <Tab> or <CR>; behave normally when menu is closed
+vim.keymap.set('i', '<Tab>', function()
+  return vim.fn.pumvisible() == 1 and '<C-y>' or '<Tab>'
+end, { expr = true })
+vim.keymap.set('i', '<CR>', function()
+  return vim.fn.pumvisible() == 1 and '<C-y>' or '<CR>'
+end, { expr = true })
 
 -- Mason / LSPs
 require("mason").setup()
